93a97a60f7ca3da0d435fbaa2cd219db4322d022
runarut <runarut@users.noreply.github.com>
2023-11-06 10:13:14 -0600

    add build/install script

---
+++ bin/Install_UltraDict.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# Install_counter.sh
+#
+
+export APP_HOME="/home/$(whoami)/UltraDict"
+export APP_PYTHON="/home/$(whoami)/.local/bin/python3.11"
+export DEBUG="True"
+
+# Debugging support
+if [ "${DEBUG}" = "True" ]
+then
+	# enable shell debug mode
+	set -x
+fi
+
+cd ${APP_HOME}
+
+if [ -d "${APP_HOME}/dist" ]
+then
+	rm -rf "${APP_HOME}/dist"
+fi
+
+${APP_PYTHON} -m build .
+ls -l dist/*.whl
+${APP_PYTHON} -m pip install --force-reinstall dist/*.whl
+
+
